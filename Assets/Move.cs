e248aab1c1f3249a65de19ca4c518216bf74b325
soonjeong <woak_uu@naver.com>
2017-05-07 16:43:05 +0900

    움직임
    오른쪽화살표_오른쪽
    왼쪽화살표_왼쪽
    항상튕기도록

---
+++ Assets/Move.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject ball;
+
+
+    void Start()
+    {
+
+    }
+	void Update () 
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 newPosition = transform.position;
+            newPosition.x += 1.0f * Time.deltaTime;
+            transform.position = newPosition;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 newPosition = transform.position;
+            newPosition.x += -1.0f * Time.deltaTime;
+            transform.position = newPosition;
+        }
+    }
+}
